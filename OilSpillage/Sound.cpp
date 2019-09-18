ec28541358474afab7604d9dc9929187bd626c3f
mrbengtsson <16099910+mrbengtsson@users.noreply.github.com>
2019-09-18 14:59:42 +0200

    Sound engine started

---
+++ OilSpillage/Sound.cpp
@@ -0,0 +1,17 @@
+#include "Sound.h"
+
+Sound Sound::instance;
+
+Sound::Sound()
+{
+	AUDIO_ENGINE_FLAGS eflags = AudioEngine_Default;
+	#ifdef _DEBUG
+	eflags = eflags | AudioEngine_Debug;
+	#endif
+
+	this->engine = AudioEngine(eflags);
+}
+
+Sound::~Sound()
+{
+}
---
+++ OilSpillage/Sound.h
@@ -0,0 +1,19 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <Audio.h>
+using namespace DirectX;
+
+class Sound
+{
+private:
+	static Sound instance;
+	AudioEngine engine;
+
+	Sound();
+
+public:
+	virtual ~Sound();
+};
+
+#endif // !SOUND_H
