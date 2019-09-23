7b1ae6e148a22bc6ac44129a569a56e3bea4a841
David Björkberg <david.bjorkberg1@gmail.com>
2019-09-23 13:36:44 +0200

    Cleaned up in actor.h

--- OilSpillage/AI/Actor.cpp
+++ OilSpillage/AI/Actor.cpp
@@ -17,98 +17,38 @@ void AIPlayer::Update(float dt)
 	followPath(dt);
 }
 
-void AIPlayer::setTarget(Vector3* target)
+void AIPlayer::setPlayerPos(Vector3 playerPos)
 {
-	this->currentTarget = target;
+	this->playerPos = playerPos;
 }
 
 void AIPlayer::findPath()
 {
-	if (currentTarget != nullptr)
-	{
-		nodePath = aStar.algorithm(position, *currentTarget);
-	}
+	//IF is chasing
+	//path = aStar.algorithm(position, playerPos);
 }
 
 void AIPlayer::followPath(float dt)
 {
-	if (nodePath.size() > 0)
+	if (path.size() > 0)
 	{
-		targetNode = DirectX::SimpleMath::Vector3(nodePath.at(nodePath.size()-1)->GetXPos(), 0, nodePath.at(nodePath.size()-1)->GetYPos());
+		targetNode = DirectX::SimpleMath::Vector3(path.at(path.size()-1)->GetXPos(), 0, path.at(path.size()-1)->GetYPos());
 		Vector3 dir = targetNode - position;
 		dir.Normalize();
 		Vector3 newPosition = position + dir * dt;
 
 		if (newPosition.Distance(targetNode, newPosition) < 1)
 		{
-			nodePath.pop_back();
+			path.pop_back();
 		}
 
 		this->setPosition(newPosition);
 	}
-
-	////check if we have a path to follow
-	//if (Path.size() > 0)
-	//{
-	//	//check if we are at the waypoint
-	//	Vector3 waypoint = this->Path.front()->wayPoint;
-	//	if (Inside(waypoint))
-	//	{
-	//		lastPosition = this->GetPositionFloat3();
-	//		// if we are inside lets pop waypoint
-	//		this->Path.pop();
-	//	}
-	//	else
-	//	{ // if we arent inside keep moving towards it
-	//		
-	//		// make a dir vector 
-	//		Vector3 dir = waypoint - lastPosition;
-	//		dir.Normalize();
-	//		Vector3 position = this->GetPositionFloat3();
-	//		Vector3 newPosition = position + dir * dt * 0.001;
-	//		this->SetPosition(newPosition);
-	//	}
-	//}
-	//else
-	//{
-	//	// if we dont have any nodes in the nodelist
-	//	
-	//	//call BT and ask for next behavoiour
-	//}
 }
-
-void AIPlayer::SetWayPoint(NodeList* Path)
-{
-	this->Path.push(Path);
-}
-
-bool AIPlayer::inRangeOf(Vector3& position)
-{
-	return this->Inside(position);
-}
-
 AIPlayer::AIPlayer()
 {
 	aStar = AStar(10, 7);
 	setPosition(DirectX::SimpleMath::Vector3(5, 0, 6));
-	setTarget(&DirectX::SimpleMath::Vector3());
+	setPlayerPos(&DirectX::SimpleMath::Vector3());
 	findPath();
-}
-
-bool AIPlayer::Inside(Vector3 waypoint)
-{
-	//check if the actor is inside the waypoint
-	bool inside = false;
-	//Calculate the squared distance from the point to the center of the sphere
-	Vector3 vecDist = waypoint - this->getPosition();
-	float fDist = sqrtf(vecDist.Dot(vecDist));
-	float Radius = 5;
-	//Calculate if the squared distance between the sphere's center and the point
-	//is less than the squared radius of the sphere
-	if (fDist < Radius * Radius)
-	{
-		inside = true;
-	}
-
-	return inside;
 }
--- OilSpillage/AI/Actor.h
+++ OilSpillage/AI/Actor.h
@@ -27,13 +27,7 @@ class AIPlayer :public GameObject
 public:
 	 
 	void Update(float dt);
-	// nodes on the grid to walk on
-	struct NodeList
-	{
-		Vector3 wayPoint;
-	};
-	// Set target
-	void setTarget(Vector3 * target);
+	void setPlayerPos(Vector3 playerPos);
 	// Move toward a target
 	void findPath();
 	// Move away from target
@@ -42,26 +36,15 @@ public:
 	void wander();
 	//Keep a certain distance from a target
 	void separate();
-	// Follow along a given path
 	void followPath(float dt);
-	// Set path List
-	void setPath(std::queue<NodeList> Path);
-	// Set path node
-	void SetWayPoint(NodeList* Path);
-	// Check if player is in line of sight
 	bool lineOfSight(Vector3 &position);
-	// Check if player is in range
-	bool inRangeOf(Vector3 & position);
 	AIPlayer();
-	//bool isEmpty() { return this->Path.size(); }
 	int getState();
 private:
-	bool Inside(Vector3 waypoint);
 	AStar aStar;
-	std::vector<Node*> nodePath;
-	std::queue<NodeList*> Path;
+	std::vector<Node*> path;
 	//use ref to  get currentTarget to use behaviour on
-	Vector3 * currentTarget;
+	Vector3 playerPos;
 	Vector3 targetNode;
 	int state;
 };
