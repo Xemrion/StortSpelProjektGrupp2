02ac62331d91df001405e80d343c3fd952e23dff
MTJohnsson <marco.t.johnsson@gmail.com>
2019-10-03 12:30:34 +0200

    Added turtlewalker network.

--- OilSpillage/States/PlayingGameState.cpp
+++ OilSpillage/States/PlayingGameState.cpp
@@ -149,7 +149,7 @@ void PlayingGameState::init()
 	this->lightList = std::make_unique<LightList>();
 	this->player = std::make_unique<Vehicle>();
 	this->camera = std::make_unique<DynamicCamera>();
-	//this->testNetwork = std::make_unique<RoadNetwork>();
+	this->testNetwork = std::make_unique<RoadNetwork>(2430, Vector2(16.0f, 16.0f), Vector2(-16.0f,-16.0f), 25); //Int seed, max pos, min pos, angle in degrees
 
 	this->graphics.loadMesh("sda");
 	this->graphics.loadShape(SHAPE_CUBE);
@@ -178,7 +178,15 @@ void PlayingGameState::init()
 	this->lightList->addLight(SpotLight(Vector3(0.f, 1.0f, 2.0f), Vector3(1.0f, 0.3f, 0.3f), 50.f, Vector3(0.f, -1.0f, 2.0f), 0.5));
 	this->lightList->addLight(SpotLight(Vector3(0.f, 1.0f, -2.0f), Vector3(0.3f, 1.0f, 0.3f), 50.f, Vector3(0.f, -1.0f, -2.0f), 0.5));
 
-
+	//Road Network Turtlewalker
+	this->testNetwork.get()->generateInitialSegments("FFFFFFFFFFFFFFF-FF-FF-FFH+F+F+FF+FF+FF+FFFFFFFFF+FF-F-FF-FFF-FFF");
+	this->testNetwork.get()->setAngle(90);
+	for (int i = 0; i < 5; i++) {
+		this->testNetwork.get()->generateAdditionalSegments("FFFF-FF+F+F+F", (i * 3) + 1, false);
+		this->testNetwork.get()->generateAdditionalSegments("H-F+FFF+F+H+F", (i * i) + 1, true);
+	}
+	//this->testNetwork.get()->cleanRoadNetwork();
+	this->testNetwork.get()->saveTestNetwork("test-network");
 
 	this->lightList->removeLight(this->lightList->addLight(PointLight(Vector3(0, 1.0f, 0.0f), Vector3(1.0f, 1.0f, 1.0f), 5000.f)));
 
@@ -249,6 +257,7 @@ void PlayingGameState::update(float deltaTime)
 	/*-------------------------RENDERING-------------------------*/
 	//Render all objects
 	this->graphics.render(this->camera.get());
+	this->testNetwork.get()->drawRoadNetwork(&this->graphics);
 
 	//ImGui rendering --BEGIN--
 	ImGui_ImplDX11_NewFrame();
