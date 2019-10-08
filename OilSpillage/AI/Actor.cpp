437ed8601b35953ef411ead3960b021c9c0c73de
David Björkberg <david.bjorkberg1@gmail.com>
2019-10-08 10:47:22 +0200

    Actor merge

--- OilSpillage/AI/Actor.cpp
+++ OilSpillage/AI/Actor.cpp
@@ -1,97 +1,13 @@
-#include "Actor.h"
 #include "..//game.h"
-void Actor::SetType(AItype type)
-{
-	this->type = type;
-}
+#include "Actor.h"
 
-Actor::AItype& Actor::GetType()
+Actor::Actor()
 {
-	return this->type;
-}
-
-void AIPlayer::Update(float dt)
-{
-	//TODO: Call findPath every X frames
-	nrOfFrames++;
-	if (nrOfFrames % 100 == 0)
+	this->setUpActor();
+	for (int i = 0; i < 3; i++)
 	{
-		//findPath();
-		nrOfFrames = 0;
-	}
-	if (path.size() == 0)
-	{
-		if (counter % 4 == 0)
-		{
-			setTargetPos(Vector3(9, 0, -9));
-		}
-		else if (counter % 4 == 1)
-		{
-			setTargetPos(Vector3(-9, 0, -9));
-		}
-		else if (counter % 4 == 2)
-		{
-			setTargetPos(Vector3(-9, 0, 9));
-		}
-		else if (counter % 4 == 3)
-		{
-			setTargetPos(Vector3(9, 0, 9));
-		}
-		counter++;
-		findPath();
-	}
-	followPath(dt);
-}
-
-void AIPlayer::setTargetPos(Vector3 targetPos)
-{
-	this->targetPos = targetPos;
-}
-
-void AIPlayer::findPath()
-{
-	if (state == AIState::chasing)
-	{
-		path = aStar->algorithm(position, targetPos);
-	}
-}
-
-void AIPlayer::followPath(float dt)
-{
-	if (path.size() > 0)
-	{
-		targetNode = Vector3(path.at(path.size() - 1)->getXPos(),
-							.0f,
-							path.at(path.size() - 1)->getYPos());
-		Vector3 dir = targetNode - position;
-		dir.Normalize();
-		Vector3 newPosition = position + dir * dt;
-		for (int i = 0; i < boids.size(); i++) //Updating Boids
-		{
-			boids.at(i)->setDestination(targetNode);
-			boids.at(i)->run(boids, dt);
-		}
-
-		if (newPosition.Distance(targetNode, newPosition) < 1)
-		{
-			path.pop_back();
-		}
-
-		this->setPosition(newPosition);
-	}
-}
-AIPlayer::AIPlayer()
-{
-	aStar = new AStar(20, 20, Vector2(-10, 10));
-	setPosition(Vector3(-9, 0, 9));
-	setTargetPos(Vector3(9,0,9));
-	state = AIState::chasing;
-	findPath();
-	for (float i = 0; i < 3; i++)
-	{
-		for (float j = 0; j < 4; j++)
-		{
-			boids.push_back(new Boid(i, j));
+		for (int j = 0; j < 4; j++) {
+			boids.push_back(new Boid(float(i), float(j)));
 		}
 
 	}
@@ -102,15 +18,204 @@ AIPlayer::AIPlayer()
 		boids.at(i)->setColor(Vector4(1.0f, 0.0f, 1.0f, 1.0f));
 		boids.at(i)->setScale(Vector3(0.5f, 0.5f, 0.5f));
 	}
-
+	this->leftoverTime = 0;
+	for (int i = 0; i < bulletCount; i++)
+	{
+		this->bullets[i].obj = new GameObject;
+		this->bullets[i].obj->mesh = Game::getGraphics().getMeshPointer("Cube");
+		this->bullets[i].obj->setScale(Vector3(0.25f, 0.25f, 0.25f));
+		this->bullets[i].obj->setColor(Vector4(1, 1, 0, 1));
+	}
 }
 
-AIPlayer::~AIPlayer()
+Actor::~Actor()
 {
-	delete aStar;
+	//this->root = nullptr;
 	for (int i = 0; i < boids.size(); i++)
 	{
 		delete boids.at(i);
 	}
 	boids.clear();
+	for (int i = 0; i < bulletCount; i++)
+	{
+		delete this->bullets[i].obj;
+	}
+}
+
+void Actor::update(float dt, Vector3 targetPos)
+{
+	this->deltaTime = dt;
+	this->targetPos = targetPos;
+
+	this->root->func();
+	followPath();
+	for (int i = 0; i < bulletCount; i++)
+	{
+		Game::getGraphics().removeFromDraw(this->bullets[i].obj);
+
+		if (this->bullets[i].timeLeft > 0.0f)
+		{
+			this->bullets[i].timeLeft -= dt;
+			this->bullets[i].obj->move(this->bullets[i].dir * this->bullets[i].speed * dt);
+			Game::getGraphics().addToDraw(this->bullets[i].obj);
+		}
+	}
+
+	for (int i = 0; i < boids.size(); i++) //Updating Boids
+	{
+		boids.at(i)->run(boids, dt);
+	}
+	this->shoot(dt, targetPos);
+}
+
+void Actor::setAStar(AStar* aStar)
+{
+	this->aStar = aStar;
+}
+
+void Actor::shoot(float deltaTime, Vector3 targetPos)
+{
+	float tempDelta = deltaTime + this->leftoverTime;
+
+	if (tempDelta <= this->weapon.fireSpeed)
+	{
+		this->leftoverTime = tempDelta;
+	}
+
+	while (tempDelta > this->weapon.fireSpeed)
+	{
+		tempDelta -= this->weapon.fireSpeed;
+		int freeToUse = 0;
+
+		while (freeToUse < bulletCount && this->bullets[freeToUse].timeLeft > 0.0f)
+		{
+			freeToUse++;
+		}
+
+		if (freeToUse < bulletCount)
+		{
+			Vector3 dir = (targetPos - this->getPosition());
+			this->bullets[freeToUse].dir = Vector3(dir.x, 0, dir.z);
+			this->bullets[freeToUse].dir.Normalize();
+			this->bullets[freeToUse].timeLeft = this->weapon.bulletLifetime;
+			this->bullets[freeToUse].speed = this->weapon.bulletSpeed;
+			this->bullets[freeToUse].obj->setPosition(this->getPosition() + Vector3(0, 2, 0));
+			this->bullets[freeToUse].obj->setRotation(Vector3(XMVector3AngleBetweenVectors(Vector3(0, 0, 1), this->bullets[freeToUse].dir)) * Vector3(0, 1, 0));
+		}
+		else
+		{
+			this->leftoverTime = 0.0f;
+			break;
+		}
+
+		if (tempDelta <= this->weapon.fireSpeed)
+		{
+			this->leftoverTime = tempDelta;
+		}
+	}
+}
+
+void Actor::chase()
+{
+	findPath();
+
+	//Vector3 dir = targetPos - this->getPosition();
+	//dir.Normalize();
+	//Vector3 newPosition = this->getPosition() + dir * deltaTime;
+	//this->setPosition(newPosition);
+
+	//for (int i = 0; i < boids.size(); i++) //Updating Boids
+	//{
+	//	boids.at(i)->setDestination(targetPos);
+	//}
+}
+void Actor::roam()
+{
+	findPath();
+	//Hitta random position och hitta väg till den.
+}
+
+Status Actor::inRange()
+{
+	Status status;
+
+	if ((getPosition() - targetPos).Length() > 10)
+	{
+		status = Status::FAILURE;
+	}
+	else
+	{
+		status = Status::SUCCESS;
+	}
+	return status;
+}
+
+Status Actor::setChaseState()
+{
+	this->state = State::Chasing;
+	return Status::SUCCESS;
+}
+
+Status Actor::setRoamState()
+{
+	this->state = State::Roaming;
+	return Status::SUCCESS;
+}
+
+void Actor::followPath()
+{
+	if (path.size() > 0)
+	{
+		targetNode = DirectX::SimpleMath::Vector3(float(path.at(path.size() - 1)->GetXPos()),
+			.0f,
+			float(path.at(path.size() - 1)->GetYPos()));
+		Vector3 dir = targetNode - this->getPosition();
+		dir.Normalize();
+		Vector3 newPosition = this->getPosition() + dir * deltaTime;
+
+		for (int i = 0; i < boids.size(); i++) //Updating Boids
+		{
+			boids.at(i)->setDestination(targetNode);
+		}
+
+		if (newPosition.Distance(targetNode, newPosition) < 1)
+		{
+			path.pop_back();
+		}
+		this->setPosition(newPosition);
+	}
+	else
+	{
+		if (this->state == State::Chasing)
+		{
+			chase();
+		}
+		else if (this->state == State::Roaming)
+		{
+			roam();
+		}
+	}
+
+}
+
+void Actor::findPath()
+{
+	aStar->algorithm(this->getPosition(), targetPos, path);
+}
+void Actor::setUpActor()
+{
+	this->root = &bt.GetSelector();
+	Sequence& sequence = bt.GetSequence();
+
+	Behavior& inRange = bt.GetAction();
+	inRange.addAction(std::bind(&Actor::inRange, std::ref(*this)));
+	Behavior& follow = bt.GetAction();
+	follow.addAction(std::bind(&Actor::setChaseState, std::ref(*this)));
+	Behavior& roam = bt.GetAction();
+	roam.addAction(std::bind(&Actor::setRoamState, std::ref(*this)));
+
+	root->AddChildren(sequence);
+	root->AddChildren(roam);
+	sequence.AddChildren(inRange);
+	sequence.AddChildren(follow);
 }
