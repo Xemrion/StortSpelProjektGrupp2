0b547e524318130c8c5b916b8c5577a1c15ebdc1
MTJohnsson <marco.t.johnsson@gmail.com>
2019-10-11 13:23:43 +0200

    Stuff

--- OilSpillage/States/PlayingGameState.h
+++ OilSpillage/States/PlayingGameState.h
@@ -5,61 +5,71 @@
 #include "../game.h"
 #include "../ImGui/imgui.h"
 #include "../vehicle.h"
-#include "../AI/Actor.h"
+#include "../AI/ActorManager.h"
 #include "../Road Network/RoadNet.h"
 #include "../DynamicCamera.h"
-#include "../PG/config.hpp"
 #include "../PG/Map.hpp"
-#include "../PG/Walker.hpp"
-#include "../PG/Map.hpp"
-#include "../PG/Voronoi.hpp"
-#include "../PG/District.hpp"
-#include "../PG/SkyscraperFloor.h"
+#include "../UI/UserInterface.h"
 
 class PlayingGameState : public GameState {
+	friend class Game;
+public:
+	enum  Menu {
+		MENU_PLAYING,
+		MENU_PAUSED,
+		MENU_OPTIONS,
+		MENUCOUNT
+	};
+
+	PlayingGameState();
+	virtual     ~PlayingGameState();
+	void         update(float delta);
+	const float& getTimeRef() const noexcept;
+	float        getTime() const noexcept;
+	void         setTime(float) noexcept;
+	void         changeTime(float delta) noexcept;
+	void         setCurrentMenu(Menu);
+	Vehicle* getPlayer() const;
+
 private:
 	friend class Game;
-   bool                            isUsingManhattanDistance = true;
-   float                           cameraDistance = 25;
-   float                           time;
-   Config                          config;
+	int                             currentMenu;
+	bool                            isUsingManhattanDistance{ true };
+	float                           cameraDistance{ 25 };
+	float                           time;
+	MapConfig                       config;
 	Graphics                       &graphics;
-	std::unique_ptr<Map>            map;
-	std::unique_ptr<Actor>          aiObject;
-	std::vector<GameObject>         districtMarkers;
-	std::vector<GameObject>         roadTiles;
-   std::vector<GameObject>         houseTiles;
+	AStar*				            aStar;
+    std::unique_ptr<Map>             map;
+	ActorManager*				    actorManager;
 	std::unique_ptr<LightList>      lightList;
 	std::unique_ptr<Vehicle>        player;
-   std::unique_ptr<Voronoi>        districtMap;
 	std::unique_ptr<DynamicCamera>  camera;
-	std::vector<CinematicPos>       points;
-	SpotLight                      *playerLight = nullptr;
-	std::unique_ptr<AStar>          aStar;
-	
-	//Test
+	std::unique_ptr<UserInterface>  menues[MENUCOUNT];
 	std::unique_ptr<RoadNetwork>    testNetwork;
-	std::unique_ptr<SkyscraperFloor> testFloor1;
-	std::unique_ptr<SkyscraperFloor> testFloor2;
+	std::vector<CinematicPos>       points;
+	SpotLight* playerLight;
+	int                             addNrOfParticles{ 2 };
+	int                             lifeTime{ 2 };
+	float                           timerForParticle{ .0f };
+	float                           vectorFieldPower{ 1.0f };
+	float                           vectorFieldSize{ 1.0f };
+	float                           randomPosPower{ 1.0f };
+	float                           size1{ .020f };
+	float                           size2{ .025f };
+	float                           colors[4]{};
+	float                           colors2[4]{};
+	float                           colors3[4]{};
+	float                           colors4[4]{};
+	Vector4                         colorsP[4]{ Vector4(1.0f, 1.0f, .0f, 1.0f),
+														Vector4(1.0f,  .0f, .0f, 1.0f),
+														Vector4(.0f,  .0f, .0f, 1.0f),
+														Vector4(.1f,  .1f, .1f, 1.0f) };
 
-	void           generateMap( Config const & );
-   void           generateBuildings( Config const &, RNG & );
-   void           ImGui_ProcGen();
-   void           ImGui_Driving();
-   void           ImGui_Camera();
-   Opt<Vec<V2u>>  find_valid_house_lot( RNG &, U16 cell_id, Voronoi const &district_map, Map &, Vec<District> const &district_tbl );
-   void           setDistrictColors(bool useColorCoding) noexcept;
-	void           initiateAStar();
-
-public:
-	               PlayingGameState();
-	virtual       ~PlayingGameState();
-	void           init();
-	void           cleanUp();
-	void           update(float deltaTime);
-	const float   &getTimeRef() const noexcept;
-	float          getTime() const noexcept;
-	void           setTime(float time) noexcept;
-	void           addTime(float time) noexcept; // TODO: slå ihop addTime och removeTime? Enda skillnaden är väl tecknet på parametern?
-	void           removeTime(float time) noexcept;
+	void initiateAStar();
+	void ImGui_ProcGen();
+	void ImGui_Driving();
+	void ImGui_Particles();
+	void ImGui_Camera();
+	void ImGui_AI();
 };
