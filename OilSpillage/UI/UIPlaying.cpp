4d30a3cdf3036a9bded97ed70db7e1998e7e74a9
mrbengtsson <16099910+mrbengtsson@users.noreply.github.com>
2019-10-30 17:01:00 +0100

    Slider 2x long

--- OilSpillage/UI/UIPlaying.cpp
+++ OilSpillage/UI/UIPlaying.cpp
@@ -103,7 +103,7 @@ UIPlaying::~UIPlaying()
 
 void UIPlaying::init()
 {
-	this->healthBar = std::make_unique<Slider>(Vector2(10, 10));
+	this->healthBar = std::make_unique<Slider>(Vector2(SCREEN_WIDTH / 2 - Slider::size.x / 2, 20));
 	this->minimap = std::make_unique<Minimap>(0.25f, 25.0f, Vector2(SCREEN_WIDTH - 10, SCREEN_HEIGHT - 10) - Minimap::size);
 	this->objectiveBox = std::make_unique<ObjectiveBox>(Vector2(0, 50));
 }
