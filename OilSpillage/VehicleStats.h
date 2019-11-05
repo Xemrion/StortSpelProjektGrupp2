bb9cb5cc55c3cf996d6e5503f277da6634cda4fc
Xemrion <xemrion@gmail.com>
2019-11-05 15:31:25 +0100

    Increased swarm speed

--- OilSpillage/VehicleStats.h
+++ OilSpillage/VehicleStats.h
@@ -25,7 +25,7 @@ public:
 									//{ health, durability, armour, accelerationRate, maxSpeed, handlingRate };
 	static constexpr Stats AITurret   = { 100,       2.0f,    2.0f,          0.0,          0.0f,      1.0f };
 									//{ health, durability, armour, accelerationRate, maxSpeed, handlingRate };
-	static constexpr Stats AISwarm    = { 20,       1.0f,    1.0f,          2.0,          5.0f,      2.0f };
+	static constexpr Stats AISwarm    = { 20,       1.0f,    1.0f,          2.0,          7.0f,      2.0f };
 };
 
 #endif // !VEHICLE_STATS_H
