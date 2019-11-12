8120ad2e89cf34ccdc95b8724fe3e93221c99122
Xemrion <xemrion@gmail.com>
2019-11-12 15:20:28 +0100

    implemented sniper, needs some work on the doging part

--- OilSpillage/VehicleStats.h
+++ OilSpillage/VehicleStats.h
@@ -21,7 +21,10 @@ public:
 
 	//AI Stats
 									//{ health, durability, armour, accelerationRate, maxSpeed, handlingRate };
+
 	static constexpr Stats AIAttacker = { 50,       2.0f,    2.0f,          0.5,          3.0f,      1.0f };
+									//{ health, durability, armour, accelerationRate, maxSpeed, handlingRate };
+	static constexpr Stats AISniper   = { 100,       3.0f,    3.0f,          1.0,          7.0f,      2.0f };
 									//{ health, durability, armour, accelerationRate, maxSpeed, handlingRate };
 	static constexpr Stats AITurret   = { 100,       2.0f,    2.0f,          0.0,          0.0f,      1.0f };
 									//{ health, durability, armour, accelerationRate, maxSpeed, handlingRate };
