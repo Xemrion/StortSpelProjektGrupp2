b51ffc814ec0a15b05b7a57ad03c53bae3dac06f
jabbathefrukt <nilsdaniellagula@gmail.com>
2019-11-26 14:17:32 +0100

    Updates - Weapon display better - mtx lock? - Fire, Smoke size fix

--- OilSpillage/Particle/ParticleSystem.cpp
+++ OilSpillage/Particle/ParticleSystem.cpp
@@ -216,6 +216,7 @@ bool ParticleSystem::addParticle(int nrOf, float lifeTime, Vector3 position, Vec
 	{
 		initialCount = -1;
 	}
+	mtx.lock();
 	pParams.initialDirection = Vector4(initialDirection.x, initialDirection.y, initialDirection.z, initialDirection.w);
 	pParams.emitterLocation = Vector4(position.x,position.y,position.z, lifeTime);
 	pParams.randomVector = Vector4(float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX,1.0f);
@@ -249,7 +250,7 @@ bool ParticleSystem::addParticle(int nrOf, float lifeTime, Vector3 position, Vec
 		deviceContext->Dispatch(nrOf, 1, 1);
 	}
 	deviceContext->CSSetUnorderedAccessViews(0, 1, &n, &initialCount);
-
+	mtx.unlock();
 	return true;
 }
 
@@ -266,6 +267,7 @@ bool ParticleSystem::addParticle(int nrOf, float lifeTime, Vector3 position, Vec
 	{
 		initialCount = -1;
 	}
+	mtx.lock();
 	pParams.initialDirection = Vector4(initialDirection.x, initialDirection.y, initialDirection.z, 1.0f);
 	pParams.emitterLocation = Vector4(position.x, position.y, position.z, lifeTime);
 	pParams.randomVector = Vector4(float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, 1.0f);
@@ -301,7 +303,7 @@ bool ParticleSystem::addParticle(int nrOf, float lifeTime, Vector3 position, Vec
 		deviceContext->Dispatch(nrOf, 1, 1);
 	}
 	deviceContext->CSSetUnorderedAccessViews(0, 1, &n, &null);
-
+	mtx.unlock();
 	return true;
 }
 
